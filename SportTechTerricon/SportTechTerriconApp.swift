2fd1e69b56b120e56d8bc0b0fa4c4a1332dbc52d
Ersultan <sabyrkhanoversultan@gmail.com>
2023-06-21 18:43:35 +0600

    Initial Commit

---
+++ SportTechTerricon/SportTechTerriconApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportTechTerriconApp.swift
+//  SportTechTerricon
+//
+//  Created by Ерсултан Сабырханов on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SportTechTerriconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
